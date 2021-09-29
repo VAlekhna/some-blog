181c437c4a57dfbfecc8841b736317fb74170251
VAlekhna <79442062+VAlekhna@users.noreply.github.com>
2021-09-29 17:33:51 +0300

    Update main.py
    deleted some comments

--- main.py
+++ main.py
@@ -221,15 +221,3 @@ def delete_post(post_id):
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
 
-
-
-# The Life of Cactus
-# Who knew that cacti lived such interesting lives.
-# October 20, 2020
-# <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery.</p>
-#
-# <p>Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
-#
-# <p>Sea lettuce lettuce water chestnut eggplant winter purslane fennel azuki bean earthnut pea sierra leone bologi leek soko chicory celtuce parsley j&iacute;cama salsify.</p>
-# https://images.unsplash.com/photo-1530482054429-cc491f61333b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80
-# Angela Yu
